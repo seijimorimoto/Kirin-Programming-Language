d44e2b7cef5b4ddd8c0ef6e3c10b0af3f918c0ab
itsStrobe <josezavala0297@gmail.com>
2018-02-27 21:45:19 -0600

    Created Original Kirin Files

---
+++ kirin_lex.py
@@ -0,0 +1,7 @@
+# Kirin Programming Language
+# Lexic File
+# Jose Juan Zavala Iglesias		| A01281362
+# Angel Seiji Morimoto Burgos	| A01281380
+
+
+import ply.lex as lex
---
+++ kirin_yacc.py
@@ -0,0 +1,6 @@
+# Kirin Programming Language
+# Syntax File
+# Jose Juan Zavala Iglesias		| A01281362
+# Angel Seiji Morimoto Burgos	| A01281380
+
+import ply.yacc as yacc
